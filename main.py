1701236e4ba5716d8efb96c149440e524cb2fa40
Pecnas <busovrm4@gmail.com>
2020-05-12 08:57:38 +0300

    ready sudoku solution

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import argparse
 
-parser   = argparse.ArgumentParser() #создаём образ парсера
+parser = argparse.ArgumentParser() #создаём образ парсера
 parser.add_argument('-c', '--color', action="store_true") # добавляем необязательный аргумент
 parser.add_argument('-vz', '--visualize', action="store_true") # добавляем необязательный аргумент
-args     = parser.parse_args()
 
-if args.color:
-	print("Color")
-if args.visualize:
-	print("visualize")
+parser.add_argument(type=str, dest="sudoku first row\n") # добавляем обязательный аргумент в виде чисел для судоку
+parser.add_argument(type=str, dest="sudoku second row\n") # добавляем обязательный аргумент в виде чисел для судоку
+parser.add_argument(type=str, dest="sudoku third row\n") # добавляем обязательный аргумент в виде чисел для судоку
+parser.add_argument(type=str, dest="sudoku fourth row\n") # добавляем обязательный аргумент в виде чисел для судоку
+
+try:
+	args = parser.parse_args()
+	if args.color:
+		print("Color")
+	if args.visualize:
+		print("visualize")
+except:
+	print("Не введены аргументы, или введены неверно!\nвызовите программу с аргументом --get_help для помощи")
--- sudoku_plan/sudoku.py
+++ sudoku_plan/sudoku.py
@@ -1,55 +1,64 @@
-class Sudoku():
-	def __init__(self, first, second, third, fourth):
-		self.first = first
-		self.second = second
-		self.third = third
-		self.fourth = fourth
-		self.row_elements = []
-	def append_elements(self):
-		self.row_elements.append(list(self.first))
-		self.row_elements.append(list(self.second))
-		self.row_elements.append(list(self.third))
-		self.row_elements.append(list(self.fourth))
-	def column_check(self, column_number): # проверяем элементы
-		column = []
-		for i in range(4): # проходимя по всем строкам
-			try:
-				if int(self.row_elements[i][column_number]) in (1, 2, 3, 4): # работаем с двумерными массивами
-					column.append(self.row_elements[i][column_number])
-				else:
-					print("Bad data")
-			except ValueError:
-				print("Full point")
-				column.append(".")
+def find_next_cell_to_fill(grid, i, j): # ищем следующую ячеку для заполнения
+		for x in range(i, 4): # проходимся по горизонтали
+			for y in range(j, 4): # проходимся по вертикали
+				if grid[x][y] == ".": # если пустая 
+					return x,y 
+		#(Процесс заполнения)
+		for x in range(0,4): # проходимся по горизонтали
+			for y in range(0,4): # проходимся по вертикали
+				if grid[x][y] == ".": # eсли равен .
+					return x,y
+		return -1,-1 # возвращаем -1 -1, когда всё заполнили
 
-		print(column)
+def is_valid(grid, i, j, e):
+	rowOk = all([e != grid[i][x] for x in range(4)]) # вернёт True если ни одно число в генерируемом списке не равно 0
+	if rowOk: # если True
+		columnOk = all([e != grid[x][j] for x in range(4)]) # вернёт True если ни одно число в генерируемом списке не равно 0
+		if columnOk: # если True
+			# начинаем искать верхний левый координат, ориентируясь на i и j
+			secTopX, secTopY = 2 * (i // 2), 2 * (j // 2) # используется этажное деление
+			for x in range(secTopX, secTopX + 2): # с помощью двух циклов проверяем квадрат 3 на 3 клетки(первый цикл)
+				for y in range(secTopY, secTopY + 2): # второй цикл
+					if grid[x][y] == e:
+						return False
+			return True
+	return False
+
+def sudoku_check(grid):
+	for x in grid:
+		past = []
+		for y in x:
+			if y in past:
+				print("[+]Bad sudoku!", y)
+			else:
+				print("[+]Good, nice Good", y)
+				past.append(y)
+		print()
 
 
+def solve_sudoku(grid, i=0, j=0): # рекурсия
+		i,j = find_next_cell_to_fill(grid, i, j) # передаём 0 и 0 (первые ячейки)
+		if i == -1: # если функция поиска ячейки вернула -1
+			return True
+		for e in range(1,5): # заполняем числами от 1 до 4(указано 5, но цикл 5 не берёт(Я запомнил это уже давно))
+			if is_valid(grid, i, j, e): # вызываем функцию 
+				grid[i][j] = e
+				if solve_sudoku(grid, i, j): # рекурсия
+					return True
 
+				grid[i][j] = 0 # обнуляем ячейку
+		return False
 
 if __name__ == "__main__":
-	sudoku = Sudoku("14.3", "3..1", ".13.", "...4")
-	sudoku.append_elements()
-	sudoku.column_check(3)
-
-
-
-
-
-#	do something...
-
+	data = [[1, 4, ".", 3], [3, ".", ".", 1], [".", 1, 3, "."], [".", ".", ".", 4]]
+	solve_sudoku(data)
+	sudoku_check(data)
+	for row in data:
+		print(row)
 '''
-"14.3" "3..1" ".13." "...4"
----------------------
-1 4 . 3
-3 . . 1
-. 1 3 .
-. . . 4
----------------------
-
-1. В каждой строке каждое число от 1 до <размер судоку> встречается один раз
-2. В каждом столбце каждое число от 1 до <размер судоку> встречается один раз
-3. В каждом блоке каждое число от 1 до <размер судоку> встречается один раз
-А также валидное судоку должно иметь только одно решение, не больше, не меньше!
+1 4 2 3
+3 2 4 1
+4 1 3 2
+2 3 1 4
 
 '''
